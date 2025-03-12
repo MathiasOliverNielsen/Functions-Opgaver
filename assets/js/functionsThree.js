d6086bff8f0158ccdbaf2a3da3629abf5c08aa3c
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-03-12 13:56:05 +0100

    opgave 3 fixed spelling

--- assets/js/functionsThree.js
+++ assets/js/functionsThree.js
@@ -1,6 +1,6 @@
 // her skal du skrive din anonyme funktion
 let myAnonFunc = function () {
-  return 'Hello World';
+  return 'hellow  World';
 };
 
 // her kaldes, eller invokes din anonyme funktion gennem en variabel
@@ -8,7 +8,7 @@ console.log(myAnonFunc());
 
 // her skal du skrive din arrow funktion
 let myArrowFunc = () => {
-  return 'Hello World';
+  return 'hellow  World';
 };
 // her kaldes, eller invokes din arrow funktion gennem en variabel
 console.log(myArrowFunc());
