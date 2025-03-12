b9c75ce96086a6969c8be9f27886d9c5a9b20120
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-03-12 13:49:43 +0100

    opgave 2.2

--- assets/js/valutaexchangeTwo.js
+++ assets/js/valutaexchangeTwo.js
@@ -1,47 +1,38 @@
-
 //Dom elementer jeg har brug for
 // knap til udregning
 const calculateButton = document.getElementById('calculateButt');
 
-
 calculateButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    //alert('Jeg vil gerne kalde en funktion der kan regne valutaen ud...');
-    gatherInputData();
+  e.preventDefault();
+  //alert('Jeg vil gerne kalde en funktion der kan regne valutaen ud...');
+  gatherInputData();
 });
-
-
-
 
 // indsamling af data fra dom (model code fra MVC)
 function gatherInputData() {
-    const myEuroRate = document.getElementById('euroRate').value;
-    const myDollarRate = document.getElementById('dollarRate').value;
+  const myEuroRate = document.getElementById('euroRate').value;
+  const myDollarRate = document.getElementById('dollarRate').value;
 
-    const myDanishAmount = document.getElementById('danishAmount').value;
+  const myDanishAmount = document.getElementById('danishAmount').value;
 
-
-
-
-    makeExchange(myEuroRate, myDollarRate, myDanishAmount);
+  makeExchange(myEuroRate, myDollarRate, myDanishAmount);
 }
-
-
-
-
-
 
 // viser resultatet  (view code fra MVC)
 function showResult(myEuroResult, myDollarResult, myDanishAmount) {
-    const resultElement = document.getElementById('result');
-    resultElement.innerHTML = myDanishAmount + ' danske kroner bliver til: ' + myEuroResult + ' euro og ' + myDollarResult + ' dollars';
-
+  const resultElement = document.getElementById('result');
+  resultElement.innerHTML = myDanishAmount + ' danske kroner bliver til: ' + myEuroResult + ' euro og ' + myDollarResult + ' dollars';
 }
 
-/* skriv din kode herunder.
+// skriv din kode herunder
+// Function 1 valutaveksling fra danske kroner til euro og dollars
+function makeExchange(myEuroRate, myDollarRate, myDanishAmount) {
+  let myDollars = calculateExchange(myDanishAmount, myDollarRate);
+  let myEuros = calculateExchange(myDanishAmount, myEuroRate);
 
-
-
-
-
-
+  showResult(myEuros, myDollars, myDanishAmount);
+}
+// Function 2 beregning af valutaveksling
+function calculateExchange(myAmount, myRate) {
+  return myAmount / myRate;
+}
